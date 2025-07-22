4c54855b1a388250532ef10a2cd54f4aa74481b8
Jaypal Dhapa <jaypaldhapa25@gmail.com>
2025-07-22 15:46:09 +0530

    Added 4 Program

---
+++ Project2-Area-of-Squarr/Area-of-Rectangle.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+    int l,b,area;
+
+    printf("Enter L = ");
+    scanf("%d",&l);
+
+    printf("Enter B = ");
+    scanf("%d",&b);
+
+    area = l*b;
+
+    printf("Area is %d",area);
+
+}
