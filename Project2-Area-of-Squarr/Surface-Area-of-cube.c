e0e9f514469aa17a6ad53123f014cb883cccb7b1
Jaypal Dhapa <jaypaldhapa25@gmail.com>
2025-07-22 15:52:15 +0530

    Added Surface-Area-of-cube program

---
+++ Project2-Area-of-Squarr/Surface-Area-of-cube.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+    int a,area;
+
+    printf("Enter A = ");
+    scanf("%d",&a);
+
+    area = 6*(a*a);
+
+    printf("Area is %d",area);
+
+}
