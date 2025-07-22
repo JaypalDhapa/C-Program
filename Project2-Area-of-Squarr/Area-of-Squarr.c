58adf62774443c8453ef875c148e2a7b006072a4
Jaypal Dhapa <jaypaldhapa25@gmail.com>
2025-07-22 15:34:07 +0530

    Added 3 Project

---
+++ Project2-Area-of-Squarr/Area-of-Squarr.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+    int l,area;
+
+    printf("Enter L = ");
+    scanf("%d",&l);
+
+    area = l*l;
+
+    printf("Area is %d",area);
+
+}
